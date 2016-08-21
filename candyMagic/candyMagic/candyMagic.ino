3a46913ce356f9affa3bb486e93b957cceb58c09
ryoyakawai <ryoya.kawai@gmail.com>
2016-08-21 14:40:22 -0700

    Add: trim dial

--- candyMagic/candyMagic/candyMagic.ino
+++ candyMagic/candyMagic/candyMagic.ino
@@ -9,12 +9,18 @@ BLEService ledService("19B10000-E8F2-537E-4F6C-984FEE0F808E"); // BLE LED Servic
 // BLE LED Characteristic - custom 128-bit UUID, read and writable by central
 BLECharCharacteristic ledCharacteristic("19B10001-E8F2-537E-4F6C-984FEE0F808E", BLERead | BLEWrite);
 
-const int ledPin = 13; // pin to use for the LED
+const int motorPin = 12; // pin to use for the Candy Magic Motor
+const int ledPin = 13; // pin to use for the led
+const int trimPin = 0;
+
+const int TRIM_VAL_MIN=151;
+const int TRIM_VAL_MAX=1023;
 
 void setup() {
   Serial.begin(9600);
 
-  // set LED pin to output mode
+  // set Motor/LED pin to output mode
+  pinMode(motorPin, OUTPUT);
   pinMode(ledPin, OUTPUT);
 
   // set advertised local name and service UUID:
@@ -40,45 +46,50 @@ void loop() {
 
   // if a central is connected to peripheral:
   if (central) {
-    Serial.print("Connected to central: ");
+    Serial.print("Connected to central:");
     // print the central's MAC address:
     Serial.println(central.address());
 
     // while the central is still connected to peripheral:
     while (central.connected()) {
       // if the remote device wrote to the characteristic,
-      // use the value to control the LED:
+      // use the value to control the time of running moter's duration:
       int duration=0;
       if (ledCharacteristic.written()) {
         if (ledCharacteristic.value()) {   // any value other than 0
           switch(ledCharacteristic.value()) {
             case 0x01:
-              duration=1000;
+              duration=10;
               break;
             case 0x02:
-              duration=2000;
+              duration=20;
               break;
             case 0x03:
-              duration=3000;
+              duration=30;
               break;
             case 0x04:
-              duration=4000;
+              duration=40;
               break;
             default:
               break;              
           }
+          // get Trim value
+          int trim = map(analogRead(trimPin), TRIM_VAL_MIN, TRIM_VAL_MAX, 0, 100);
           Serial.println(duration);
 
+          digitalWrite(motorPin, HIGH);         // will turn the motor on
           digitalWrite(ledPin, HIGH);         // will turn the LED on
-          delay(duration);
+          delay(duration * trim);
+          digitalWrite(motorPin, LOW);          // will turn the motor off
           digitalWrite(ledPin, LOW);          // will turn the LED off
-          Serial.println(F("LED off"));
+          Serial.println("LED off");
+
         }
       }
     }
 
     // when the central disconnects, print it out:
-    Serial.print(F("Disconnected from central: "));
+    Serial.print("Disconnected from central.");
     Serial.println(central.address());
   }
 }
