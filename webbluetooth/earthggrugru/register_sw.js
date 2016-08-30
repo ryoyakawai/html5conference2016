667377dfece775663aea8ee77a3fcef4157a55b3
ryoyakawai <ryoya.kawai@gmail.com>
2016-08-30 03:11:10 -0700

    Update

--- webbluetooth/earthggrugru/register_sw.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function() {
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('./sw.js').then(function(registration) {
-            console.log('ServiceWorker: Registration Succeed. Scope: ', registration.scope);
-        }).catch(function(err) {
-            console.log('ServiceWorker: Registration Failed.  Message: ', err);
-        });
-    }
-});
