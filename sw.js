706bd2a91c239a58af0d9e67182d4dd12c527586
ryoyakawai <ryoya.kawai@gmail.com>
2016-09-02 11:22:28 +0900

    Tweak

--- sw.js
+++ sw.js
@@ -9,18 +9,18 @@ var urlsToCache = [
     './sw.js',
 
     './js/material.min.js',
-    './webbluetooth/candyMagic/md/icon_family_material_icons.css',
-    './webbluetooth/candyMagic/md/material.green-light_green.min.css',
-    './webbluetooth/candyMagic/md/material.min.js',
-    './webbluetooth/candyMagic/md/fonts/2fcrYFNaTjcS6g4U3t-Y5StnKWgpfO2iSkLzTz-AABg.ttf',
-    './webbluetooth/candyMagic/md/fonts/2fcrYFNaTjcS6g4U3t-Y5UEw0lE80llgEseQY3FEmqw.woff2',
-    './webbluetooth/candyMagic/md/fonts/hMqPNLsu_dywMa4C_DEpY14sYYdJg5dU2qzJEVSuta0.woff2',
-    './webbluetooth/candyMagic/md/fonts/hMqPNLsu_dywMa4C_DEpY1BW26QxpSj-_ZKm_xT4hWw.woff2',
-    './webbluetooth/candyMagic/md/fonts/hMqPNLsu_dywMa4C_DEpY4gp9Q8gbYrhqGlRav_IXfk.woff2',
-    './webbluetooth/candyMagic/md/fonts/hMqPNLsu_dywMa4C_DEpY6E8kM4xWR1_1bYURRojRGc.woff2',
-    './webbluetooth/candyMagic/md/fonts/hMqPNLsu_dywMa4C_DEpY9DiNsR5a-9Oe_Ivpu8XWlY.woff2',
-    './webbluetooth/candyMagic/md/fonts/hMqPNLsu_dywMa4C_DEpY_ZraR2Tg8w2lzm7kLNL0-w.woff2',
-    './webbluetooth/candyMagic/md/fonts/hMqPNLsu_dywMa4C_DEpYwt_Rm691LTebKfY2ZkKSmI.woff2'
+    './webbluetooth/candymagic/md/icon_family_material_icons.css',
+    './webbluetooth/candymagic/md/material.green-light_green.min.css',
+    './webbluetooth/candymagic/md/material.min.js',
+    './webbluetooth/candymagic/md/fonts/2fcrYFNaTjcS6g4U3t-Y5StnKWgpfO2iSkLzTz-AABg.ttf',
+    './webbluetooth/candymagic/md/fonts/2fcrYFNaTjcS6g4U3t-Y5UEw0lE80llgEseQY3FEmqw.woff2',
+    './webbluetooth/candymagic/md/fonts/hMqPNLsu_dywMa4C_DEpY14sYYdJg5dU2qzJEVSuta0.woff2',
+    './webbluetooth/candymagic/md/fonts/hMqPNLsu_dywMa4C_DEpY1BW26QxpSj-_ZKm_xT4hWw.woff2',
+    './webbluetooth/candymagic/md/fonts/hMqPNLsu_dywMa4C_DEpY4gp9Q8gbYrhqGlRav_IXfk.woff2',
+    './webbluetooth/candymagic/md/fonts/hMqPNLsu_dywMa4C_DEpY6E8kM4xWR1_1bYURRojRGc.woff2',
+    './webbluetooth/candymagic/md/fonts/hMqPNLsu_dywMa4C_DEpY9DiNsR5a-9Oe_Ivpu8XWlY.woff2',
+    './webbluetooth/candymagic/md/fonts/hMqPNLsu_dywMa4C_DEpY_ZraR2Tg8w2lzm7kLNL0-w.woff2',
+    './webbluetooth/candymagic/md/fonts/hMqPNLsu_dywMa4C_DEpYwt_Rm691LTebKfY2ZkKSmI.woff2'
 
 ];
 
